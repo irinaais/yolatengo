229b3a8077783e72e8b2037fb8759b41a5dafcea
Irina Shorokhova <irinaais90@gmail.com>
2024-09-18 13:59:17 +0300

    feat: add WelcomePage

--- src/ui/pages/WelcomePage/WelcomePage.tsx
+++ src/ui/pages/WelcomePage/WelcomePage.tsx
@@ -1,9 +1,31 @@
 import { ReactElement } from "react";
+import "./WelcomePage.css";
+import {Link} from "react-router-dom";
 
 export default function WelcomePage(): ReactElement {
   return (
-    <div className='welcome'>
-      <h1>Стартовая страница</h1>
-    </div>
+    <main className='welcome column'>
+      <Link className='link' to={'/search'}>
+        <div className='welcome_title-line' data-text="yo">yo</div>
+        <div className='row'>
+          <div className='column'>
+            <p>with special guests</p>
+            <p>the schramms and kustomized</p>
+          </div>
+          <div className='welcome_title-line' data-text="la">la</div>
+        </div>
+        <div className='welcome_title-line' data-text="ten-">ten-</div>
+        <div className='row flex-end'>
+          <div className='column gap'>
+            <p>saturday / november 27 1993</p>
+            <div>
+              <p>at cbgb and omfug</p>
+              <p>315 bowery at bleecker st / nyc</p>
+            </div>
+          </div>
+          <div className='welcome_title-line' data-text="go">go</div>
+        </div>
+      </Link>
+    </main>
   );
 }
